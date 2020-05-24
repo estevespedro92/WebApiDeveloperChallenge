dec268a3e1a73b49251bbf5498cb604ef11a2c61
dev.homelounge <dev.homelounge@hotmail.com>
2020-05-24 14:02:01 +0200

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDeveloperChallenge.Models
+{
+  public class Contact
+  {
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Fullname { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string MobilePhoneNumber { get; set; }
+  }
+}
